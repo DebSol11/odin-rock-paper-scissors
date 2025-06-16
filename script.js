76679490b0589b633d1225e479e04a6c518a3331
Michael <100490812+DebSol11@users.noreply.github.com>
2025-06-16 14:30:58 +0200

    Write the logic to play a single round
    Writing the logic to play a single round of Rock, Paper, Scissors.

--- script.js
+++ script.js
@@ -38,9 +38,69 @@ function getComputerChoice() {
   -store the user input in a variable
   -return that variable to use it's value */
 function getHumanChoice() {
-  let userInput = prompt("Please choose Rock, Paper or Scissor by typing it into the field");
+  let userInput = prompt(
+    "Please choose Rock, Paper or Scissor by typing it into the field"
+  );
   return userInput;
 }
 
-console.log("You chose " + getHumanChoice());
-console.log("The computer chose " + getComputerChoice());
+// console.log("You chose " + getHumanChoice());
+// console.log("The computer chose " + getComputerChoice());
+
+/*Problem solving
+  1. Understand the problem:
+      How to write the code to play a single round of rock, paper, scissors against a NPC 
+      (≡ = Definition = Non Player Character)?
+  2. Plan:
+      - The code will be played in the console.
+      - The user inputs Rock, Paper or Scissors.
+      - The desired output is an updated score of the human and the computer, which is than displayed in the console.
+      - If you have the two choices than you need to compare them and given the rules of Rock, Paper, Scissors decide who get's a score.
+  3. Pseudocode: 
+      - Create a new function named playRound.
+      - Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+      - Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+      - Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+      - Increment the humanScore or computerScore variable based on the round winner.*/
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  let displayChoices = 
+  `You chose ${humanChoice}. 
+  The computer chose ${computerChoice}.`;
+  if (humanChoice == computerChoice) {
+    return console.log(
+      `You both chose the same.
+      This is a draw!`);
+  } else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+    return console.log(
+      `${displayChoices}
+      You lose. PAPER beats ROCK!`);
+  } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+    return console.log(
+      `${displayChoices}
+      You win. ROCK beats SCISSORS!`);
+  } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+    return console.log(
+      `${displayChoices}
+      You win. PAPER beats ROCK!`);
+  } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+    return console.log(
+      `${displayChoices}
+      You lose. SCISSORS cut PAPER!`);
+  } else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
+    return console.log(
+      `${displayChoices}
+      You lose. ROCK destroys SCISSORS!`);
+  } else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+    return console.log(
+      `${displayChoices}
+      You win. SCISSORS cut PAPER in pieces!`);
+  } else {
+    return console.log("Oh noooo! Something must have gone wrong!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
